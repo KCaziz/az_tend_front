69461a0029d4c32caef23917fc6ef1f74c2fb620
KCaziz <114615343+KCaziz@users.noreply.github.com>
2024-09-22 18:40:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path';
 
 export default defineConfig({
-    server: {
-    host: '44.226.145.213', // Remplacer par 'localhost' si nécessaire
-    port: 5173, // Port du serveur de développement React
-  },
   plugins: [react()],
   resolve: {
     alias: {
