5d1b8070e5fa8f5a70dc7521c1d0183024f4ba17
KCaziz <114615343+KCaziz@users.noreply.github.com>
2024-09-22 18:26:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path';
 
 export default defineConfig({
+    server: {
+    host: 'https://az-tend-front.onrender.com', // Remplacer par 'localhost' si nécessaire
+    port: 5173, // Port du serveur de développement React
+  },
   plugins: [react()],
   resolve: {
     alias: {
